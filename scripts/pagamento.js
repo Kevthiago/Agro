7ab927e2783f6d77248755a84bf493e362d35bfa
Kevthiago <145407238+Kevthiago@users.noreply.github.com>
2024-10-02 12:38:44 -0300

    att

--- scripts/pagamento.js
+++ scripts/pagamento.js
@@ -1,35 +1,3 @@
-class Burguer {
-    constructor() {
-        this.itens = document.getElementById('itens');
-    }
-
-    mudouTamanho() {
-        if (window.innerWidth <= 768) {
-            this.itens.style.display = 'none';
-        } else {
-            this.itens.style.display = 'block';
-        }
-    }
-
-    clickMenu() {
-        if (this.itens.style.display === 'block') {
-            this.itens.style.display = 'none';
-        } else {
-            this.itens.style.display = 'block';
-        }
-    }
-}
-
-const menuBurguer = new Burguer();
-
-// Adicionar um evento de clique ao ícone do hambúrguer usando JavaScript
-document.getElementById('burguer').addEventListener('click', () => menuBurguer.clickMenu());
-
-// Adicionar evento de redimensionamento e carregamento da página
-window.onresize = () => menuBurguer.mudouTamanho();
-window.onload = () => menuBurguer.mudouTamanho();
-
-
 class Pagamento {
     constructor() {
         this.pagamentoSelect = document.getElementById('lista-pagamento');
@@ -59,4 +27,3 @@ class Pagamento {
 }
 
 const pagamento = new Pagamento();
-
